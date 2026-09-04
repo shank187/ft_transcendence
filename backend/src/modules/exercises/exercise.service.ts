12891ad46e91f289b1952927a5f93509625ae9f7
shank187 <aymaneelbour33@gmail.com>
2026-09-04 20:50:04 +0100

    feat(exercise-catalog): implement exercise retrieval functionality with controller and service layers

---
+++ backend/src/modules/exercises/exercise.service.ts
@@ -0,0 +1,47 @@
+import { prisma } from "../../app";
+
+
+export async function getExercises() {
+    const results = await prisma.exercise.findMany({
+        select:{
+            id: true,
+            name: true,
+            primaryMuscleGroup: {
+                select:{
+                    name: true,
+                },
+            },
+            secondaryMuscles: {
+                select:{
+                    muscleGroup:{
+                        select:{
+                            name: true,
+                        },
+                    },
+                },
+            },
+            equipment: {
+                select:{
+                    equipment:{
+                        select:{
+                            name: true,
+                        },
+                    },
+                },
+            },
+        },
+    });
+
+    // DTO
+    const cleanExercises = results.map((exercise) => {
+        return{
+            id: exercise.id,
+            name: exercise.name,
+            primaryMuscle: exercise.primaryMuscleGroup.name,
+            secondaryMuscles: exercise.secondaryMuscles.map((item) => item.muscleGroup.name),
+            equipments: exercise.equipment.map((item) => item.equipment.name),
+            
+        };
+    });
+    return cleanExercises;
+}
