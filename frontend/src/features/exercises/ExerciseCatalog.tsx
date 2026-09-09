8722a8846c46a382036b180a1c1d263a8c4fc239
shank187 <aymaneelbour33@gmail.com>
2026-09-09 10:56:10 +0100

    fix(exercises): complete catalog card display

--- frontend/src/features/exercises/ExerciseCatalog.tsx
+++ frontend/src/features/exercises/ExerciseCatalog.tsx
@@ -32,6 +32,7 @@ function ExerciseCatalog() {
         return(
             <section>
                 <h1>{error}</h1>
+                
             </section>
         )
     else if (exercises.length === 0)
@@ -43,7 +44,7 @@ function ExerciseCatalog() {
     return (
         <section>
         <h1>Exercises Count: {exercises.length}</h1>
-        <ul>
+        <div>
             {exercises.map(exercise => {
                 return (
                     <ExerciseCard
@@ -52,7 +53,7 @@ function ExerciseCatalog() {
                     />
                 )
             })}
-        </ul>
+        </div>
         </section>
     )
 }
