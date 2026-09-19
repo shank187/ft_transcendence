d81a9ea181714c02ba6bd0dcc1377fe4e826a677
Mohamed Ouarar <mouarar@e2r9p7.1337.ma>
2026-09-19 09:26:45 +0100

    feat(chat): handle websocket disconnections

--- backend/src/modules/chat/chat.logic.ts
+++ backend/src/modules/chat/chat.logic.ts
@@ -2,6 +2,9 @@ import { WebSocketServer } from 'ws'
 
 export function initializeChat(wss: WebSocketServer) {
     wss.on('connection', (ws, req) => {
-        console.log("client connected.")
+        console.log(`client connected.`)
+        ws.on('close', (code , reason) => {
+            console.log(`connection closed with ${code} and reason: ${reason}`)
+        })
     })
 }
